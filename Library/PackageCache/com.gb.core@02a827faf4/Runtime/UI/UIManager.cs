fb1dc86247b59dcd16cba5f65ffe898dd2700a33
Han9067 <901130h@naver.com>
2025-09-27 12:31:50 +0900

    250927
    ui매니저 수정

--- Library/PackageCache/com.gb.core@02a827faf4/Runtime/UI/UIManager.cs
+++ Library/PackageCache/com.gb.core@02a827faf4/Runtime/UI/UIManager.cs
@@ -293,18 +293,18 @@ namespace GB
         }
 
 
-        // void OnBackKey()
-        // {
-        //     if (_popupList.Count > 0)
-        //     {
-        //         _popupList[0].BackKey();
-        //     }
-        //     else
-        //     {
-        //         if (_scene != null)
-        //             _scene.BackKey();
-        //     }
-        // }
+        void OnBackKey()
+        {
+            if (_popupList.Count > 0)
+            {
+                _popupList[0].BackKey();
+            }
+            else
+            {
+                if (_scene != null)
+                    _scene.BackKey();
+            }
+        }
 
         public static void ClosePopup()
         {
@@ -337,14 +337,14 @@ namespace GB
             _popupList.Sort((s1, s2) => s2.Weight.CompareTo(s1.Weight));
         }
 
-        // private void Update()
-        // {
-        //     if (Input.GetKeyDown(KeyCode.Escape))
-        //     {
-        //         if (canClose)
-        //             OnBackKey();
-        //     }
-        // }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (canClose)
+                    OnBackKey();
+            }
+        }
     }
 
     public static class UiUtil
