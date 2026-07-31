b3b16bf6194e447850c1167845aeca5757a868d4
Han9067 <901130h@naver.com>
2026-07-31 11:20:37 +0900

    260731
    아이템 리소스 옥의티 수정

--- Assets/Scripts/Manager/PlayerManager.cs
+++ Assets/Scripts/Manager/PlayerManager.cs
@@ -164,7 +164,7 @@ public class PlayerManager : AutoSingleton<PlayerManager>
         // ItemManager.I.CreateInvenItem(40001, -1, -1); //양손둔기
         // ItemManager.I.CreateInvenItem(46001, -1, -1); //창
         // ItemManager.I.CreateInvenItem(50001, -1, -1); //활
-        ItemManager.I.CreateInvenItem(10001, -1, -1); //옷
+        ItemManager.I.CreateInvenItem(10005, -1, -1); //옷
         // ItemManager.I.CreateInvenItem(10061, -1, -1); //갑옷
         // ItemManager.I.CreateInvenItem(54001, -1, -1); //화살
         pData.EqSlot["Hand1"] = pData.Inven[0]; // 손1
